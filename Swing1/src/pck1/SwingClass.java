215cd712ad043747fea53169632ba0d07b5ac689
Jivesh <jvjivesh@gmail.com>
2022-02-19 12:10:22 +0530

    Swing Addition Program

--- Swing1/src/pck1/SwingClass.java
+++ Swing1/src/pck1/SwingClass.java
@@ -14,6 +14,7 @@ public class SwingClass  {
 		fr.setVisible(true);
 		fr.setSize(300,299);
 		fr.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		fr.getContentPane().setBackground(Color.CYAN);
 		
 		JButton b1;
 		JLabel l1,l2,l3;
@@ -23,10 +24,13 @@ public class SwingClass  {
 		
 		fr.add(l1);
 		l1.setBounds(0,90,130,30);
+		l1.setFont(new Font("Rozha One",Font.BOLD,10));
 //		Second Label
 		l2=new JLabel("Enter Second Number:");
 		fr.add(l2);
 		l2.setBounds(10,100,100,30);
+		l2.setFont(new Font("",Font.BOLD,10));
+		
 //		First TextField
 		t1=new JTextField();
 		fr.add(t1);
@@ -40,7 +44,7 @@ public class SwingClass  {
 //		third TextField
 		t3=new JTextField();
 		fr.add(t3);
-		t3.setBounds(100,150,70,20);
+		t3.setBounds(100,150,100,20);
 		t3.setEditable(false);
 		
 //		button
@@ -50,12 +54,36 @@ public class SwingClass  {
 		b1.setBounds(100,180,70,20);
 		b1.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				long c=0;
 				String s1=t1.getText();
 				String s2=t2.getText();
-				int a=Integer.parseInt(s1);
-				int b=Integer.parseInt(s2);
-				int c=0;
+				try {
+				long a=Long.parseLong(s1);
+				long b=Long.parseLong(s2);
+				
+				
+				
+				
+					
+				
+				
+					
+				
 				c=a+b;
+				}
+				
+				
+				catch(NumberFormatException e2)
+				{
+					System.out.println("NumberFormatException occurred");
+					System.out.println("Invailid Input");
+					System.out.println("Please Enter Vailid Input");
+					
+				}
+				
+				
+				
+				
 				String result=String.valueOf(c);
 				t3.setText(result);
 			}
